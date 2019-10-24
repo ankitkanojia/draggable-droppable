43662371a434d885405dd297cf33376d50f9c9cb
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-24 22:54:56 +0530

    Added draggable and droppable

--- draggable-droppable/src/App.js
+++ draggable-droppable/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-// import DragThingsToBoxesDemo from './DragThingsToBoxes/DragThingsToBoxesDemo';
+import DragThingsToBoxesDemo from './DragThingsToBoxes/DragThingsToBoxesDemo';
 import BasicDragDemo from './BasicDragDemo';
 import DragFoodToAnimalsDemo from './DragFoodToAnimals/DragFoodToAnimalsDemo';
+import DragElementWithClickHandlers from './DragElementWithClickHandlers';
 
 function App() {
   return (
     <div className="App">
+      <DragThingsToBoxesDemo/>
+				<hr />
       <DragFoodToAnimalsDemo/>
 				<hr />
       <BasicDragDemo />
+      <hr />
+				<DragElementWithClickHandlers />
     </div>
   );
 }
