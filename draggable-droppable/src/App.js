9d68de4db03ffbf923c9d8f36a918dfbab66ad33
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-27 13:47:39 +0530

    Update the dropdown plugin

--- draggable-droppable/src/App.js
+++ draggable-droppable/src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import DragThingsToBoxesDemo from './DragThingsToBoxes/DragThingsToBoxesDemo';
-import BasicDragDemo from './BasicDragDemo';
-import DragFoodToAnimalsDemo from './DragFoodToAnimals/DragFoodToAnimalsDemo';
-import DragElementWithClickHandlers from './DragElementWithClickHandlers';
 
 function App() {
   return (
     <div className="App">
-      <DragThingsToBoxesDemo />
-      <hr />
-      <DragFoodToAnimalsDemo />
-      <hr />
-      <BasicDragDemo />
-      <hr />
-      <DragElementWithClickHandlers />
+      <label>APP</label>
     </div>
   );
 }
