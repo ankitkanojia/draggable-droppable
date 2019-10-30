5723a6cc86626d065212345aa5ce2b525deb113f
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-30 16:12:11 +0530

    images added & package update
    Sample images added to perform drag and drop functionality.
    Update the package react-dnd to react-draggable in current application.

--- draggable-droppable/src/App.js
+++ draggable-droppable/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css'
+import Draggable from "y";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="leftDiv">
-          Hello
+          <Draggable><img height="100" src={require("./img/gorilla.png")} /></Draggable>
+          <img height="100" src={require("./img/panda.jpg")} />
+          <img height="100" src={require("./img/puppy.png")} />
         </div>
         <div className="rightDiv">
-          Hello
+          <img height="100" src={require("./img/gorilla.png")} />
+          <img height="100" src={require("./img/panda.jpg")} />
+          <img height="100" src={require("./img/puppy.png")} />
         </div>
       </div>
     );
