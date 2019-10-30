8114354714dbfa180e46eeda5e628ba664a5cd18
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-30 15:54:15 +0530

    Two divs added into app.js
    Two containers are added into home screen.

--- draggable-droppable/src/App.js
+++ draggable-droppable/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <label>APP</label>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="leftDiv">
+          Hello
+        </div>
+        <div className="rightDiv">
+          Hello
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
