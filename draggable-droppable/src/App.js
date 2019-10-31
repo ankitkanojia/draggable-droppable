a273d9908ba42c37027945830953a02532e3fbba
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-31 22:06:16 +0530

    Added bound to element

--- draggable-droppable/src/App.js
+++ draggable-droppable/src/App.js
@@ -5,7 +5,8 @@ import Draggable from "react-draggable";
 class App extends React.Component {
 
   state = {
-    leftWidth : 0
+    leftWidth : 0,
+    controlledPosition: { left : 8, top : 8, right : 480, bottom : 175.60000610351562 }
   }
 
 
@@ -16,21 +17,21 @@ class App extends React.Component {
   }
 
   handleStop = (index) => {
-    const imagewidh = this.refs["img_" + index].getBoundingClientRect().width + this.refs["img_" + index].getBoundingClientRect().x;
-    if(imagewidh < this.state.leftWidth)
-    {
-      console.log("inside");
-    }else{
-      console.log("outside");
-    }
+    // const imagewidh = this.refs["img_" + index].getBoundingClientRect().width + this.refs["img_" + index].getBoundingClientRect().x;
+    // if(imagewidh < this.state.leftWidth)
+    // {
+    //   console.log("inside");
+    // }else{
+    //   console.log("outside");
+    // }
   }
 
   render() {
     return (
       <div className="App">
         <div className="leftDiv" ref={"leftDiv"}>
-          <Draggable onStop={() => this.handleStop(1)}><img ref={"img_1"} height="100" src={require("./img/gorilla.png")} /></Draggable>
-          <Draggable onStop={() => this.handleStop(2)}><img ref={"img_2"} height="100" src={require("./img/panda.jpg")} /></Draggable>
+          <Draggable bounds={this.state.controlledPosition} onStop={() => this.handleStop(1)}><img ref={"img_1"} height="100" src={require("./img/gorilla.png")} /></Draggable>
+          <Draggable position={this.state.controlledPosition} onStop={() => this.handleStop(2)}><img ref={"img_2"} height="100" src={require("./img/panda.jpg")} /></Draggable>
           <Draggable onStop={() => this.handleStop(3)}><img ref={"img_3"} height="100" src={require("./img/puppy.png")} /></Draggable>
         </div>
         <div className="rightDiv">
