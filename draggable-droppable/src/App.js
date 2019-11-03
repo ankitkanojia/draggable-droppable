cfac05e2e38b88a9ca0de2b1037b0fc79d8f5fd8
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-03 19:52:42 +0530

    Added position of each images so that identify the postion of images on specific box

--- draggable-droppable/src/App.js
+++ draggable-droppable/src/App.js
@@ -1,25 +1,25 @@
-  import React from 'react';
+import React from 'react';
 import './App.css'
 import Draggable from "react-draggable";
 
 class App extends React.Component {
 
   state = {
-    leftWidth : 0,
+    leftWidth: 0,
     controlledLeftPosition: [
-      { left : -32, top : -32, right : 1388, bottom : 36 },
-      { left : -132, top : -32, right : 1288, bottom : 36 },
-      { left : -232, top : -32, right : 1188, bottom : 36 }
+      { left: -32, top: -32, right: 1388, bottom: 36, position: "left" },
+      { left: -132, top: -32, right: 1288, bottom: 36, position: "left" },
+      { left: -232, top: -32, right: 1188, bottom: 36, position: "left" }
     ],
     controlledRightPosition: [
-      { left : -880, top : -32, right : 540, bottom : 36 },
-      { left : -980, top : -32, right : 440, bottom : 36 },
-      { left : -1080, top : -32, right : 340, bottom : 36 }
+      { left: -880, top: -32, right: 540, bottom: 36, position: "right" },
+      { left: -980, top: -32, right: 440, bottom: 36, position: "right" },
+      { left: -1080, top: -32, right: 340, bottom: 36, position: "right" }
     ]
   }
 
 
-  componentDidMount(){
+  componentDidMount() {
     console.log(this.refs["leftDiv"].getBoundingClientRect());
     console.log(this.refs["rightDiv"].getBoundingClientRect());
     // this.setState({
@@ -29,10 +29,9 @@ class App extends React.Component {
 
   handleStop = (index) => {
     const imagewidh = this.refs["img_" + index].getBoundingClientRect().width + this.refs["img_" + index].getBoundingClientRect().x;
-    if(imagewidh < this.state.leftWidth)
-    {
+    if (imagewidh < this.state.leftWidth) {
       console.log("inside");
-    }else{
+    } else {
       console.log("outside");
     }
   }
@@ -50,6 +49,22 @@ class App extends React.Component {
           <Draggable bounds={this.state.controlledRightPosition[1]}><img height="100" src={require("./img/panda.jpg")} /></Draggable>
           <Draggable bounds={this.state.controlledRightPosition[2]}><img height="100" src={require("./img/puppy.png")} /></Draggable>
         </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <ul style={{marginTop : 200}}>
+          {this.state.controlledLeftPosition.map((data, index) => {
+            return <li key={index + 1}>{index + 1} {data.position}</li>
+          })}
+        </ul>
+        <br />
+        <ul>
+          {this.state.controlledRightPosition.map((data, index) => {
+            return <li key={index + 1}>{index + 1} {data.position}</li>
+          })}
+        </ul>
       </div>
     );
   }
